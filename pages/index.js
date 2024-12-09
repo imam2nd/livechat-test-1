392378253147fdf8bb3b8bfe193b915e82a3be5c
imam2nd <imammahadi651@gmail.com>
2024-12-09 11:51:58 +0600

    chore: loading testing script for robi team

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,7 @@ function Masuma() {
           id="myAliceWebChat"
           dangerouslySetInnerHTML={{
             __html: `
-!function(){var e=document.createElement("div");e.id="myAliceWebChat";var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://myalice-webchat-v2.netlify.app/index.js";var a=document.body.getElementsByTagName("script");(a=a[a.length-1]).parentNode.insertBefore(t,a),a.parentNode.insertBefore(e,a),t.addEventListener("load",function(){MyAliceWebChat.init({selector:"#myAliceWebChat",  platformId: '21438',primaryId: 'c79457165eb411efac3466c4aa8b4f06',token: '0b4df78b4612b16bcf92b8db2a36a677cd9a450d617d7f38',})})}();
-  `,
+!function(){var e=document.createElement("div");e.id="myAliceWebChat";var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://livechat.myalice.ai/index.js";var a=document.body.getElementsByTagName("script");(a=a[a.length-1]).parentNode.insertBefore(t,a),a.parentNode.insertBefore(e,a),t.addEventListener("load",function(){MyAliceWebChat.init({selector:"#myAliceWebChat",platformId:"21438",primaryId:"c79457165eb411efac3466c4aa8b4f06",token:"0b4df78b4612b16bcf92b8db2a36a677cd9a450d617d7f38"})})}();   `,
           }}
         />
       </footer>
